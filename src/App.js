85142b47be09a87d0775b45606f1f35cfe8012c0
Deep <deep.arya3198@gmail.com>
2024-06-18 20:28:45 +0530

    second upload

--- src/App.js
+++ src/App.js
@@ -4,21 +4,26 @@ import './App.css';
 function App() {
   const [city, setCity] = useState('');
   const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const fetchWeather = async () => {
+    setLoading(true);
     const apiKey = 'c44112c3aa67d9b3b3808ae183a04e53';
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
     );
     const data = await response.json();
     setWeather(data);
+    setCity(''); // Clear the input box after fetching data
+    setLoading(false);
   };
 
   const handleInputChange = (event) => {
     setCity(event.target.value);
   };
 
-  const handleSearch = () => {
+  const handleSearch = (event) => {
+    event.preventDefault();
     fetchWeather();
   };
 
@@ -27,23 +32,26 @@ function App() {
       <section>
         <p id="heading"><span className="fancy">WEATHER FORECAST</span></p>
         <div className="container">
-          <input
-            type="text"
-            value={city}
-            onChange={handleInputChange}
-            placeholder="Enter city name"
-          />
-          <button onClick={handleSearch}>Search</button>
+          <form onSubmit={handleSearch}>
+            <input
+              type="text"
+              value={city}
+              onChange={handleInputChange}
+              placeholder="Enter city name"
+            />
+            <button type="submit">Search</button>
+          </form>
         </div>
-        {weather && <div className="weather-location">{weather.name}</div>}
+        {loading && <div className="loading">Loading...</div>}
+        {weather && !loading && <div className="weather-location">{weather.name}</div>}
         <div className="containerTop">
-          {weather && (
+          {weather && !loading && (
             <div className="weather-info">
               <h2>Temperature</h2>
               <p>{weather.main.temp}°C</p>
             </div>
           )}
-          {weather && (
+          {weather && !loading && (
             <div className="weather-info1">
               <h2>Weather</h2>
               <p>{weather.weather[0].description}</p>
@@ -51,16 +59,16 @@ function App() {
           )}
         </div>
         <div className="containerBottom">
-          {weather && (
+          {weather && !loading && (
             <div className="weather-info2">
               <h2>Humidity</h2>
               <p>{weather.main.humidity}%</p>
             </div>
           )}
-          {weather && (
+          {weather && !loading && (
             <div className="weather-info3">
               <h2>Wind Speed</h2>
-              <p>{weather.wind.speed} m/s</p>
+              <p>{weather.wind.speed} m/s}</p>
             </div>
           )}
         </div>
